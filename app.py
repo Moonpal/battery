92ee18da84c095498f3a929bd7409775a7116e5b
Moonpal <109739649+Moonpal@users.noreply.github.com>
2023-12-04 23:17:04 +0900

    10초간격으로 PCA분석 시행 및  동적으로 Flask 서버에  변형 결과 업데이트

--- app.py
+++ app.py
@@ -43,32 +43,7 @@ def send_data():
             cursor.execute(query)
             # 가져온 데이터를 JSON 형식으로 변환
             data = cursor.fetchone()
-
-##################################################################################################################################
-# Way1
-            # if data:
-            #     df = pd.DataFrame([data])
-            #     sliced_df = df.iloc[:, 23:]
-            #     accumulated_df = pd.concat([accumulated_df, sliced_df], ignore_index=True)
-
-            #     # 최초 10초 동안 누적된 데이터에 대해 주성분 분석 수행
-            #     if len(accumulated_df) <= 10:
-            #         if len(accumulated_df) == 10:
-            #             processed_df = diff_smooth_df(accumulated_df.copy(), lags_n=0, diffs_n=0, smooth_n=0)
-            #             processed_df = do_pca(processed_df, 3)
-            #             print(processed_df)
-            #             json_data = processed_df.to_json(orient='records')
-            #             socketio.emit('update_table', {'data': json_data}, namespace='/test', room=None, skip_sid=None)
-
-            #     # 1초 간격으로 데이터가 누적될 때마다 주성분 분석 수행
-            #     elif len(accumulated_df) % 10 == 0:
-            #         processed_df = diff_smooth_df(accumulated_df.copy(), lags_n=0, diffs_n=0, smooth_n=0)
-            #         processed_df = do_pca(processed_df, 3)
-            #         print(processed_df)
-            #         json_data = processed_df.to_json(orient='records')
-            #         socketio.emit('update_table', {'data': json_data}, namespace='/test', room=None, skip_sid=None)
-###################################################################################################################################           
-# way2
+     
             if data:
                 # 데이터프레임으로 변환
                 df = pd.DataFrame([data])
@@ -80,21 +55,18 @@ def send_data():
                 accumulated_df = pd.concat([accumulated_df, sliced_df], ignore_index=True)
                 print(accumulated_df)
 
-                 # 10초 이후부터 1초 간격으로 PCA 수행
-                if len(accumulated_df) > 10 and (len(accumulated_df) - 10) % 10 == 0:
+                # 10개씩 주기적으로 PCA 수행
+                if len(accumulated_df) >= 10 and len(accumulated_df) % 10 == 0:
                     processed_df = diff_smooth_df(accumulated_df.copy(), lags_n=0, diffs_n=0, smooth_n=0)
                     processed_df = do_pca(processed_df, 3)
                     print(processed_df)
 
-                    # time_segment 함수 적용
-
                     # 전처리된 데이터를 JSON 형식으로 변환
                     json_data = processed_df.to_json(orient='records')
-                    
 
                     # 소켓을 통해 데이터를 모든 클라이언트로 전송
                     socketio.emit('update_table', {'data': json_data}, namespace='/test', room=None, skip_sid=None)
-###############################################################################################################################
+
             last_time = data['Time']
             db.commit()
 
@@ -107,6 +79,67 @@ def send_data():
     finally:
         cursor.close()
 
+##############################################################################################################################
+# # 데이터를 전송받는 함수 구축
+# def send_data():
+#     global accumulated_df
+
+#     try:
+#         cursor = db.cursor()
+
+#         # 처음에는 last_time을 None으로 설정하여 가장 처음에 받은 데이터의 시간으로 초기화
+#         last_time = None
+
+#         while True:
+#             # 데이터베이스에서 다음 데이터 가져오기
+#             if last_time is None:
+#                 query = "SELECT * FROM test01_ok_chg ORDER BY Time ASC LIMIT 1"
+#             else:
+#                 query = f"SELECT * FROM test01_ok_chg WHERE Time > '{last_time}' ORDER BY Time ASC LIMIT 1"
+            
+#             # 쿼리문 실행
+#             cursor.execute(query)
+#             # 가져온 데이터를 JSON 형식으로 변환
+#             data = cursor.fetchone()
+     
+#             if data:
+#                 # 데이터프레임으로 변환
+#                 df = pd.DataFrame([data])
+
+#                 # 여기에서 데이터를 슬라이싱하여 원하는 열(24번째 열부터)까지 추출
+#                 sliced_df = df.iloc[:, 23:]
+
+#                 # 원본 데이터프레임에 현재 데이터 누적
+#                 accumulated_df = pd.concat([accumulated_df, sliced_df], ignore_index=True)
+#                 print(accumulated_df)
+
+#                  # 10초 이후부터 1초 간격으로 PCA 수행
+#                 if len(accumulated_df) > 10 and (len(accumulated_df) - 10) % 10 == 0:
+#                     processed_df = diff_smooth_df(accumulated_df.copy(), lags_n=0, diffs_n=0, smooth_n=0)
+#                     processed_df = do_pca(processed_df, 3)
+#                     print(processed_df)
+
+#                     # time_segment 함수 적용
+
+#                     # 전처리된 데이터를 JSON 형식으로 변환
+#                     json_data = processed_df.to_json(orient='records')
+                    
+
+#                     # 소켓을 통해 데이터를 모든 클라이언트로 전송
+#                     socketio.emit('update_table', {'data': json_data}, namespace='/test', room=None, skip_sid=None)
+
+#             last_time = data['Time']
+#             db.commit()
+
+#             # 1초간격으로 데이터 갱신
+#             socketio.sleep(1)
+
+#     except pymysql.Error as e:
+#         print(e)
+
+#     finally:
+#         cursor.close()
+##############################################################################################################################
 
 # 백그라운드 스레드에서 데이터를 실시간으로 전송하는 함수 실행
 @socketio.on('connect', namespace='/test')
