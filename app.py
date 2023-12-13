06f3bbcc827d271f6d63f296ce69ef9f9420c4d3
Moonpal <109739649+Moonpal@users.noreply.github.com>
2023-12-13 12:08:12 +0900

    시각화 여러개  합류전 코드

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from sklearn.preprocessing import MinMaxScaler
 
 from data_processing import *
 from gan_models import *
-from gan_models import build_encoder_layer, build_generator_layer, build_critic_x_layer, build_critic_z_layer
 from anomaly_detection import Anomaly
 
 from keras.models import Model
@@ -15,93 +14,8 @@ from keras.optimizers import Adam
 
 import pandas as pd
 import pymysql
-import json
-import matplotlib
 
-
-#######################################################################################################################
-## HyperParameter
-win_size = 100
-features_dim = 3
-feat_dim = features_dim
-params = {}
-params['plot_network']=True
-params['epochs']=30
-params['batch_size']=64
-params['n_critic']=5
-params['learning_rate']=0.00005
-params['latent_dim']=20
-params['shape'] = [win_size, features_dim]
-params['encoder_input_shape'] = [win_size, features_dim]
-params['encoder_reshape_shape'] = [20, 1]
-params['generator_input_shape'] = [20, 1]
-params['generator_reshape_shape'] = [win_size, 1]
-params['critic_x_input_shape'] = [win_size, features_dim]
-params['critic_z_input_shape'] = [20, 1]
-print("win_size = %d, features_dim = %d " % (win_size, features_dim))
-
-## layer parameter
-shape = params['shape'] # [win_size = 10, features_dim = 3]
-window_size = shape[0]
-feat_dim = shape[1]
-latent_dim = params['latent_dim'] # params['latent_dim']	= 20
-encoder_input_shape = params['encoder_input_shape']
-generator_input_shape = params['generator_input_shape']
-critic_x_input_shape = params['critic_x_input_shape']
-critic_z_input_shape = params['critic_z_input_shape']
-encoder_reshape_shape = params['encoder_reshape_shape']
-generator_reshape_shape = params['generator_reshape_shape']
-
-# 레이어 구축
-# encoder_input_shape = [20, 1], encoder_reshape_shape = [20,1]
-encoder = build_encoder_layer(input_shape=encoder_input_shape,
-encoder_reshape_shape=encoder_reshape_shape)
-
-# generater_input_shape = [20,1], generator_reshape_shape = [10,1]
-generator = build_generator_layer(input_shape=generator_input_shape, generator_reshape_shape=generator_reshape_shape)
-
-# critic_x_input_shape = [10, 3]
-critic_x = build_critic_x_layer(input_shape=critic_x_input_shape)
-
-# critic_z_input_shape = [20, 1]
-critic_z = build_critic_z_layer(input_shape=critic_z_input_shape)
-optimizer = tf.keras.optimizers.legacy.Adam(learning_rate) #레거시 옵티마이저 사용으로 변경
-
-## 생성한 4가지 모델 layer로 critic_x_model, critic_z_model, interpolated_x, interpolated_z
-z = Input(shape=(latent_dim, 1)) # (20, 1)
-x = Input(shape=shape) # (10,3)
-x_ = generator(z) # build_generator_layer(20,1) -> return (10,3) shape의 Generator(Decoding) Data
-z_ = encoder(x) # build_encoder_layer(10,3) -> return (20,1) shape의 Encoding Data
-
-fake_x = critic_x(x_) # (None,1) shape 가짜 데이터
-valid_x = critic_x(x) # (None,1) shape 진짜 데이터
-
-# fake_x, valid_x 데이터 선형 결합
-interpolated_x = RandomWeightedAverage(batch_size)([x, x_])
-critic_x_model = Model(inputs=[x, z], outputs=[valid_x, fake_x, interpolated_x])
-
-fake_z = critic_z(z_) # Decoding 된 (None,1) data -> critic_z_layer
-valid_z = critic_z(z) # Encoding 된 (None,1) data -> shape critic_z_layer
-
-# fake_z, valid_z 데이터 선형 결합
-interpolated_z = RandomWeightedAverage(batch_size)([z, z_])
-critic_z_model = Model(inputs=[x, z], outputs=[valid_z, fake_z, interpolated_z])
-z_gen = Input(shape=(latent_dim, 1))
-x_gen_ = generator(z_gen)
-x_gen = Input(shape=shape)
-z_gen_ = encoder(x_gen)
-x_gen_rec = generator(z_gen_)
-fake_gen_x = critic_x(x_gen_)
-fake_gen_z = critic_z(z_gen_)
-encoder_generator_model = Model([x_gen, z_gen], [fake_gen_x, fake_gen_z, x_gen_rec])
-
-### 가중치 모델 적용
-critic_x_model.load_weights('C:/Users/user/BusanDigitalAcademy/Battery_Project/critic_x_model.h5')
-critic_z_model.load_weights('C:/Users/user/BusanDigitalAcademy/Battery_Project/critic_z_model.h5')
-encoder_generator_model.load_weights('C:/Users/user/BusanDigitalAcademy/Battery_Project/encoder_generator_model.h5')
-
-# #######################################################################################################################
-
+## arguments 정의
 arguments=collections.namedtuple('Args',
  'signal_file timest_form anomaly_file mode aggregate_interval regate_interval')
 args=arguments(signal_file='/content/drive/MyDrive/충방전 데이터파일/data/raw_data/test/Test07_NG_dchg.csv',
@@ -230,11 +144,22 @@ def send_data():
                         anomalies, length_anom, X, Z_score1 = process_anomaly_detection(X, y_hat, critic, X_index)
                         if anomalies is not None:
 
+                            # 전압,온도 데이터 추출
+                            vol_df = accumulated_df.iloc[:,:176]
+                            tem_df = accumulated_df.iloc[:,176:,]
+                            print(vol_df)
+                            print(tem_df)
+
+                            
                             # 시각화 함수 호출
+                            image_path_vol = save_vol_plot_to_file(vol_df)
+                            image_path_tem = save_tem_plot_to_file(tem_df)
                             image_path = save_plot_to_file(anomalies, length_anom, X, Z_score1)
+                            
                             # 이미지 파일의 경로를 클라이언트에 전송
                             socketio.emit('update_plot', {'image_path': image_path}, namespace='/test')
-                
+                            socketio.emit('update_plot_vol', {'image_path': image_path_vol}, namespace='/test')
+                            socketio.emit('update_plot_tem', {'image_path': image_path_tem}, namespace='/test')
                             
                     else:
                         # 예외가 발생하거나 데이터가 충분하지 않은 경우에 대한 처리
@@ -244,7 +169,7 @@ def send_data():
             db.commit()
 
             # 1초 간격으로 데이터 갱신
-            socketio.sleep(0.01)
+            socketio.sleep(0.1)
 
     except pymysql.Error as e:
         print(e)
@@ -263,7 +188,7 @@ def test_connect():
 # Flask 라우트 설정
 @app.route('/')
 def index():
-    return render_template('index copy.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
